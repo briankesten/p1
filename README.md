8691ac357e4adbb99ba5787927a67cf92fafac42
Brian Kestenholz <briankesten@yahoo.com>
2015-09-10 20:18:24 -0700

    Project 1 First Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Project 1 Course Landing Page
+
+## Live URL
+<http://p1.netcaredata.com>
+
+## Description
+This project is the landing page for the CSCI E-15 course projects for Brian Kestenholz. It contains all the details related to each project.
+
+## Demo
+Not applicable to project one
+
+## Details for teaching team
+Project one uses a simple bootstrap theme with a dropdown navigation bar for easy navigation to each project.  
+
+## Outside code
+* Bootstrap: http://getbootstrap.com/
+* JQuery: https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js
+* Creative Common Images:  http://onlinehelp.microsoft.com/en-us/bing/dn261810.aspx
+* Shim: https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js
+* Respond: https://oss.maxcdn.com/respond/1.4.2/respond.min.js
---
+++ index.php
@@ -0,0 +1,250 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brian Kestenholz
+ * Date: 9/10/2015
+ * Time: 6:18 PM
+ * Used Bootstrap Template Start Kit
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Brian Kestenholz | CSCI E-15 | Class Project Page</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="container">
+
+    <!-- Static navbar -->
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">CSCI E-15 | Brian Kestenholz | Class Projects</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Class Projects<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="README.md">Project 1 README.md</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#project-2">Project 2</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#project-3">Project 3</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#project-4">Project 4</a></li>
+                        </ul>
+                    </li>
+                </ul>
+
+            </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+    </nav>
+
+    <!-- Main component for a primary marketing message or call to action -->
+    <div class="jumbotron">
+        <img src="img/family.jpg" class="img-responsive" alt="Family Picture">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <h1>Brian Kestenholz</h1>
+                <p>Brian Kestenholz lives in San Antonio, Texas with his wife and three children.    He is a director at Panasonic where he manages consulting and support teams.  He has limited experience with programming through his MIS undergraduate and graduate degrees and is seeking to expand his web programming knowledge by completing the Web Technologies Certificate.    During his career he has developed small software projects in C#, PHP, Perl, Javascript, VBScript, and Powershell languages, but has never held a position as a software developer.    Throughout this course he will be using a Windows 10 x64 VMware VM to complete his coursework.</p>
+            </div>
+        </div>
+    </div>
+
+</div> <!-- /container -->
+<!-- Wrap the rest of the page in another container to center all the content. -->
+
+<div class="container marketing">
+
+    <!-- START THE FEATURETTES -->
+
+    <hr class="featurette-divider">
+
+    <div id="project-2" class="row featurette">
+        <div class="col-md-7">
+            <h3 class="featurette-heading">Project 2: <span class="text-muted">XKCD Style Password Generator</span></h3>
+
+            <p class="lead">This web development project will focus on creating a XKCD Style Password Generator. Below
+                is the information for Project 2. </p>
+            <!-- Table -->
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Project Deliverable</th>
+                    <th>Link</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Website</td>
+                    <td><a href="http://p2.netcaredata.com">http://p2.netcaredata.com</a></td>
+
+                </tr>
+                <tr>
+                    <td>GitHub.com SSH Address</td>
+                    <td>git@github.com:briankesten/p2.git</td>
+                </tr>
+                <tr>
+                    <td>GitHub.com HTTPs Address</td>
+                    <td><a href="https://github.com/briankesten/p2.git">https://github.com/briankesten/p2.git</a></td>
+                </tr>
+                <tr>
+                    <td>Screencast</td>
+                    <td><a href="http://p2.netcaredata.com/screencast.mp4">http://p2.netcaredata.com/screencast.mp4</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>README.md File</td>
+                    <td><a href="http://p2.netcaredata.com/README.md">http://p2.netcaredata.com/README.md</a>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-5">
+            <img class="featurette-image img-responsive center-block" width="70%" height="70%" src="img/lock.png"
+                 alt="Password Generator">
+        </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div id="project-3" class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+            <h3 class="featurette-heading">Project 3: <span class="text-muted">Developer's Best Friend</span></h3>
+
+            <p class="lead">This web development project will focus on utilizing the Laravel framework. Below
+                is the information for Project 3. </p>
+            <!-- Table -->
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Project Deliverable</th>
+                    <th>Link</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Website</td>
+                    <td><a href="http://p3.netcaredata.com">http://p3.netcaredata.com</a></td>
+
+                </tr>
+                <tr>
+                    <td>GitHub.com SSH Address</td>
+                    <td>git@github.com:briankesten/p3.git</td>
+                </tr>
+                <tr>
+                    <td>GitHub.com HTTPs Address</td>
+                    <td><a href="https://github.com/briankesten/p3.git">https://github.com/briankesten/p3.git</a></td>
+                </tr>
+                <tr>
+                    <td>Screencast</td>
+                    <td><a href="http://p3.netcaredata.com/screencast.mp4">http://p3.netcaredata.com/screencast.mp4</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>README.md File</td>
+                    <td><a href="http://p3.netcaredata.com/README.md">http://p3.netcaredata.com/README.md</a>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+            <img class="featurette-image img-responsive center-block" src="img/developer.png"
+                 alt="Project 3 Image">
+        </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div id="project-4" class="row featurette">
+        <div class="col-md-7">
+            <h3 class="featurette-heading">Project 4: <span class="text-muted">Final Project</span></h3>
+
+            <p class="lead">This is the final project for CSCI E-15. Below
+                is the information for Project 4. </p>
+            <!-- Table -->
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Project Deliverable</th>
+                    <th>Link</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Website</td>
+                    <td><a href="http://p4.netcaredata.com">http://p4.netcaredata.com</a></td>
+
+                </tr>
+                <tr>
+                    <td>GitHub.com SSH Address</td>
+                    <td>git@github.com:briankesten/p4.git</td>
+                </tr>
+                <tr>
+                    <td>GitHub.com HTTPs Address</td>
+                    <td><a href="https://github.com/briankesten/p4.git">https://github.com/briankesten/p4.git</a></td>
+                </tr>
+                <tr>
+                    <td>Screencast</td>
+                    <td><a href="http://p4.netcaredata.com/screencast.mp4">http://p4.netcaredata.com/screencast.mp4</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>README.md File</td>
+                    <td><a href="http://p4.netcaredata.com/README.md">http://p4.netcaredata.com/README.md</a>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-5">
+            <img class="featurette-image img-responsive center-block" src="img/final.jpg"
+                 alt="Project 4">
+        </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <!-- /END THE FEATURETTES -->
+
+
+    <!-- FOOTER -->
+    <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+
+        <p>&copy; 2015 Brian Kestenholz. &middot; </p>
+    </footer>
+
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
